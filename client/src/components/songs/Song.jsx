8cab03fa0c6ab7a40ecb442300d76649e0761fe0
Finn Stitcher <foundfootage@protonmail.com>
2023-04-08 22:10:03 -0700

    links display where relevant

--- client/src/components/songs/Song.jsx
+++ client/src/components/songs/Song.jsx
@@ -1,17 +1,34 @@
-function Song({song}) {
-    const {_id, title, artist, album, year} = song;
+function Song({ song }) {
+	const { _id, title, artist, album, year, links } = song;
 
 	return (
-	    <li className="my-0" id={_id}>
-	        <p className="font-medium">{title}</p>
+		<li className="my-0" id={_id}>
+			<p className="font-medium">{title}</p>
 
-	        <p className="text-neutral-700 font-normal">
-	            {artist}
-	            {year && (`, ${year}`)}
-	            {album && <>, <i>{album}</i></>}
-	        </p>
-	    </li>
-	)
+			<p className="text-neutral-700 font-normal">
+				{artist}
+				{year && `, ${year}`}
+				{album && (
+					<>
+						, <i>{album}</i>
+					</>
+				)}
+			</p>
+
+			<p className="text-neutral-700 font-normal">
+                Links:{" "}
+				{links &&
+					links.map((element, index) => (
+						<>
+							<a key={index} href={element.href}>
+								{element.source}
+							</a>
+                            {index < links.length - 1 && <span>, </span>}
+						</>
+					))}
+			</p>
+		</li>
+	);
 }
 
 export default Song;
